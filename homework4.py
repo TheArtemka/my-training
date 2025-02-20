92294ed8dda48d4883ac9866710ae937a1d95ec8
TheArtemka <artemkagd005@gmail.com>
2025-02-20 22:11:37 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,6 @@
+name = input("Введите ваше имя: ")
+print("Привет,", name)
+current_year = 2025
+date_of_birth = int(input("какой ваш год рождения? "))
+age = current_year - date_of_birth
+print("В этом году вам", age)
